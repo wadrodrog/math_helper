9b19e9fce851f2d94ef86c04c7ccc846f6592f7d
wadrodrog <135006964+wadrodrog@users.noreply.github.com>
2024-12-14 09:30:40 +0300

    Documentation

--- lib/permutations/permutation.go
+++ lib/permutations/permutation.go
@@ -1,21 +1,12 @@
-/*
-	Math Helper - implementation of common algorithms
-
-# Copyright (C) 2024  wadrodrog
-
-This program is free software: you can redistribute it and/or modify
-it under the terms of the GNU General Public License as published by
-the Free Software Foundation, either version 3 of the License, or
-(at your option) any later version.
-
-This program is distributed in the hope that it will be useful,
-but WITHOUT ANY WARRANTY; without even the implied warranty of
-MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-GNU General Public License for more details.
-
-You should have received a copy of the GNU General Public License
-along with this program.  If not, see <https://www.gnu.org/licenses/>.
-*/
+// Пакет permutations предоставляет реализацию алгоритмов перестановок:
+//   - Создание перестановки
+//   - Подсчёт количества перестановок
+//   - Подсчёт количества инверсий
+//   - Определение чётности перестановки
+//   - Разложение на циклы
+//   - Разложение на транспозиции
+//   - Сборка перестановки из транспозиции
+//   - Умножение перестановок
 package permutations
 
 func allNumbersFrom1ToN(n int, slice []int) error {
@@ -32,30 +23,27 @@ func allNumbersFrom1ToN(n int, slice []int) error {
 	return nil
 }
 
-// Permutation function
+// Permutation представляет собой перестановку.
 type Permutation struct {
-	size           int         // How many elements this permutation has
-	arguments      []int       // Permutation arguments
-	values         []int       // Permutation values
-	associations   map[int]int // Arguments - values
-	inversions     int         // Pairs of positions (i, j) where the entries of a permutation are in the opposite order
-	count          uint        // Factorial of size
-	cycles         [][]int     // Decomposed permutation into cycles
-	transpositions [][]int     // Decomposed permutation into cycles of length 2
+	size           int         // Количество элементов в перестановке (n)
+	arguments      []int       // Аргументы перестановки (верхний ряд чисел)
+	values         []int       // Значения перестановки (нижний ряд чисел)
+	associations   map[int]int // Ассоциации: Аргумент - Значение
+	inversions     int         // Инверсии: пары индексов (i, j), при которых элементы перестановки расположены в обратном порядке.
+	count          uint        // Количество перестановок (факториал количества элементов)
+	cycles         [][]int     // Разложение перестановки на циклы
+	transpositions [][]int     // Разложение перестановки на транспозиции (циклы длины 2)
 }
 
-// Returns Permutation.
+// Возвращает перестановку. Перестановка задаётся двумя массивами чисел
+// от 1 до n.
 //
-// Permutation arguments and values are unordered arrays of all numbers
-// from 1 to n
+// Возвращает ошибку, если в массивах нет всех чисел от 1 до n.
 //
-// Returns error if each slice does not have all numbers from 1 to n.
+// Пример:
 //
-// Example:
-//
-// (3 2 1)
-//
-// (2 1 3)
+//	(2 1 3)
+//	(3 2 1)
 func NewPermutation(n int, arguments []int, values []int) (*Permutation, error) {
 	if len(arguments) != n {
 		return nil, InvalidLengthError(len(arguments), n)
@@ -79,21 +67,15 @@ func NewPermutation(n int, arguments []int, values []int) (*Permutation, error) 
 	return &Permutation{n, arguments, values, associations, -1, 0, [][]int{}, [][]int{}}, nil
 }
 
-// Returns Permutation.
+// Возвращает перестановку. Перестановка задаётся массивом чисел от 1 до n.
+// Аргументы перестановки (верхний ряд чисел) упорядочен.
 //
-// Permutation arguments is an ordered array of all numbers from 1 to n, defined
-// automatically.
+// Возвращает ошибку, если в массиве нет всех чисел от 1 до n.
 //
-// Permutation values is an unordered array of all numbers from 1 to n, defined
-// manually by passing a slice.
+// Пример:
 //
-// Returns error if values slice does not have all numbers from 1 to n.
-//
-// Example:
-//
-// (3 2 1)
-//
-// (1 2 3)
+//	(1 2 3)
+//	(2 1 3)
 func NewSequencePermutation(n int, values []int) (*Permutation, error) {
 	arguments := make([]int, n)
 	for i := 0; i < n; i++ {
@@ -102,18 +84,17 @@ func NewSequencePermutation(n int, values []int) (*Permutation, error) {
 	return NewPermutation(n, arguments, values)
 }
 
-// Returns Permutation.
+// Возвращает перестановку. Перестановка составляется из транспозиций.
+// Аргументы перестановки (верхний ряд чисел) упорядочен.
 //
-// Permutation arguments is an ordered array of all numbers from 1 to n, defined
-// automatically.
+// Возвращает ошибку, если невозможно составить перестановку из заданных
+// транспозиций.
 //
-// Permutation values is an unordered array of all numbers from 1 to n, defined
-// from transpositions cyctels.
+// Пример:
 //
-// Returns error if it is impossible to construct permutation with numbers
-// from 1 to n.
+//	(1 3)(3 2)(2 1) => (1 3 2)
 func NewSequencePermutationFromTranspositions(transpositions [][]int) (*Permutation, error) {
-	// Find max value
+	// Находим максимальное значение
 	maxValue := -1
 	for i := 0; i < len(transpositions); i++ {
 		if len(transpositions[i]) != 2 || transpositions[i][0] < 1 || transpositions[i][1] < 1 {
@@ -125,7 +106,7 @@ func NewSequencePermutationFromTranspositions(transpositions [][]int) (*Permutat
 		return nil, InvalidTranspositionError(0)
 	}
 
-	// Create arguments slice using found max value
+	// Создаём массив аргументов, используя найденное максимальное значение
 	arguments := make([]int, maxValue)
 	values := make([]int, maxValue)
 	for i := 0; i < maxValue; i++ {
@@ -133,7 +114,7 @@ func NewSequencePermutationFromTranspositions(transpositions [][]int) (*Permutat
 		values[i] = i + 1
 	}
 
-	// Recreate values slice using transpositions cycles
+	// Восстанавливаем массив значений, меняя числа местами согласно транспозициям
 	for i := 0; i < len(transpositions); i++ {
 		x := transpositions[i][0] - 1
 		y := transpositions[i][1] - 1
@@ -145,16 +126,14 @@ func NewSequencePermutationFromTranspositions(transpositions [][]int) (*Permutat
 	return NewPermutation(maxValue, arguments, values)
 }
 
-// Returns inversions count of current permutation.
-//
-// Calculates the value for the first time, then caches it.
+// Возвращает количество инверсий перестановки.
 func (p *Permutation) Inversions() int {
-	// Return cached value
+	// Возващаем кэшированное значение
 	if p.inversions != -1 {
 		return p.inversions
 	}
 
-	// Calculate for the first time
+	// Вычисляем значение в первый раз
 	p.inversions = 0
 	for i := 0; i < p.size; i++ {
 		for j := i + 1; j < p.size; j++ {
@@ -166,23 +145,21 @@ func (p *Permutation) Inversions() int {
 	return p.inversions
 }
 
-// Returns true if the permutation's transpositions count is even
+// Возвращает true, если количество транспозиций перестановки чётно.
 func (p *Permutation) IsEven() bool {
 	return len(p.Transpositions())%2 == 0
 }
 
-// Returns permutations count of n elements. It is n!
+// Возвращает количество перестановок n элементов.
 //
-// Calculates the value for the first time, then caches it.
-//
-// This methods will run slowly and return incorrect result for big values.
+// Вычисляет факториал из n медленно и неправильно для больших значений.
 func (p *Permutation) Count() uint {
-	// Return cached value
+	// Возващаем кэшированное значение
 	if p.count != 0 {
 		return p.count
 	}
 
-	// Calculate for the first time
+	// Вычисляем значение в первый раз
 	p.count = 1
 	for i := uint(1); i <= uint(p.size); i++ {
 		p.count *= i
@@ -190,66 +167,62 @@ func (p *Permutation) Count() uint {
 	return p.count
 }
 
-// Returns permutation decomposition into cycles.
-//
-// Calculates the value for the first time, then caches it.
+// Возвращает разложение перестановки на циклы.
 func (p *Permutation) Cycles() [][]int {
-	// Return cached value
+	// Возващаем кэшированное значение
 	if len(p.cycles) != 0 {
 		return p.cycles
 	}
 
 	used := make([]bool, p.size)
 
-	// Build cycles
+	// Строим циклы
 	for i := 0; i < p.size; i++ {
-		// Shortcuts
+		// Сокращения
 		arg := p.arguments[i]
 		val := p.associations[arg]
 
-		// Do not start a cycle if initial value already has been written
+		// Не начинать цикл, если начальное значение уже было использовано
 		if used[arg-1] {
 			continue
 		}
 
-		// This will be the cycle of length 1. They are usually omitted
+		// Это будет цикл длины 1. Обычно их не записывают
 		if arg == val {
 			continue
 		}
 
-		// Otherwise, start a cycle with first two values
+		// Иначе начинаем цикл с первыми двумя значениями
 		cycle := []int{arg, val}
 		used[arg-1] = true
 		used[val-1] = true
 
-		// Build a cycle step-by-step
-		// Every step is value of previous as index
-		// Repeat until the value does not equal initial value
+		// Пошаговое построение цикла.
+		// Каждый шаг - это значение от предыдущего аргумента.
+		// Повторять, пока значение не равно первоначальному.
 		for j := p.associations[val]; j != p.arguments[i]; j = p.associations[j] {
 			cycle = append(cycle, j)
 			used[j-1] = true
 		}
 
-		// Write a cycle
+		// Записать цикл
 		p.cycles = append(p.cycles, cycle)
 	}
 
 	return p.cycles
 }
 
-// Returns permutation decomposition into transpositions cycles.
-//
-// Calculates the value for the first time, then caches it.
+// Возвращает разложение перестановки на транспозиции.
 func (p *Permutation) Transpositions() [][]int {
-	// Return cached value
+	// Возващаем кэшированное значение
 	if len(p.transpositions) != 0 {
 		return p.transpositions
 	}
 
-	// Calculate cycles
+	// Сначала нужно разложить на циклы
 	p.Cycles()
 
-	// Build transpositions cycles
+	// Строим транспозиции
 	for i := 0; i < len(p.cycles); i++ {
 		for j := 1; j < len(p.cycles[i]); j++ {
 			p.transpositions = append(p.transpositions, []int{p.cycles[i][j-1], p.cycles[i][j]})
@@ -259,16 +232,17 @@ func (p *Permutation) Transpositions() [][]int {
 	return p.transpositions
 }
 
-// Returns permutations composition. Only sequence permutations supported.
+// Возвращает композицию перестановок. Это умножение текущей перестановки на
+// другую.
 //
-// Returns error if permutations lengths does not equal
+// Возвращает ошибку, если длины перестановок не равны.
 func (p1 Permutation) Multiply(p2 Permutation) (*Permutation, error) {
-	// Permutations length should be equal
+	// Проверка длины
 	if p1.size != p2.size {
 		return nil, InvalidLengthError(p2.size, p1.size)
 	}
 
-	// Calculate new permutation
+	// Вычислить новую перестановку
 	values := make([]int, p1.size)
 	for i := 0; i < p2.size; i++ {
 		val := p2.associations[p2.arguments[i]]
